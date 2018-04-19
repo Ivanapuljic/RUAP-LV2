5f3de66efa17da7db92dec62738acb25be7de21e
test <test@test123.com>
2018-04-19 09:59:57 +0200

    task2

---
+++ Begin/ContactManager/Areas/HelpPage/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactManager.Areas.HelpPage.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /HelpPage/Contact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string[] Get()
+        {
+            return new string[]
+    {
+        "Hello",
+        "World"
+    };
+        }
+    }
+    
+}
